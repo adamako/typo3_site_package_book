100c3760e378dea90f02502834e646ac30f17fad
Adama KO <adamako34@hotmail.com>
2024-04-11 17:46:43 +0000

    base design

--- Resources/Public/JavaScript/Src/main.js
+++ Resources/Public/JavaScript/Src/main.js
@@ -1,1 +1,15 @@
 console.log('WE LOVE TYPO3');
+$(document).ready(function() {
+    $('.tx-felogin-pi1 form fieldset div').each(function(id) {
+        $(this).addClass('form-group');
+    })
+    $('.tx-felogin-pi1 form fieldset div input').each(function(id) {
+        var type = $(this).attr('type');
+        if (type == 'text' || type == 'password') {
+            $(this).addClass('form-control');
+        }
+        if (type == 'submit') {
+            $(this).addClass('btn btn-primary');
+        }
+    });
+});
